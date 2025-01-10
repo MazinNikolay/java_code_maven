19f85af3de582e6ff848fb7321623b120bfbd2a3
kolia <koliamba_85@mail.ru>
2025-01-10 16:40:10 +0800

    Task complete

--- src/main/java/com/javacode/learn/App.java
+++ src/main/java/com/javacode/learn/App.java
@@ -1,9 +1,8 @@
 package com.javacode.learn;
 
 import com.javacode.learn.model.Shape;
-import com.javacode.learn.model.impl.Circle;
-import com.javacode.learn.model.impl.Rectangle;
-import com.javacode.learn.model.impl.Triangle;
+import com.javacode.learn.model.ThreeDShape;
+import com.javacode.learn.model.impl.*;
 
 public class App {
     public static void main(String[] args) {
@@ -13,11 +12,30 @@ public class App {
 
         System.out.println("circle.getArea() = " + circle.getArea());
         System.out.println("circle.getPerimeter() = " + circle.getPerimeter());
+        System.out.println("circle.getShapeName() = " + circle.getShapeName());
 
         System.out.println("rectangle.getArea() = " + rectangle.getArea());
         System.out.println("rectangle.getPerimeter() = " + rectangle.getPerimeter());
+        System.out.println("rectangle.getShapeName() = " + rectangle.getShapeName());
 
         System.out.println("triangle.getArea() = " + triangle.getArea());
         System.out.println("triangle.getPerimeter() = " + triangle.getPerimeter());
+        System.out.println("triangle.getShapeName() = " + triangle.getShapeName());
+
+        System.out.println(ComparingShapes.isEqualShapesType(circle, rectangle));
+
+        Shape rectangle2 = new Rectangle(6.0, 7.0);
+        System.out.println(ComparingShapes.isEqualShapesType(rectangle2, rectangle));
+
+        ComparingShapes.getShapeInfo(circle);
+        ComparingShapes.getShapeInfo(rectangle2);
+        ComparingShapes.getShapeInfo(rectangle);
+        ComparingShapes.getShapeInfo(triangle);
+
+        ThreeDShape cube = new Cube(5.0);
+        ThreeDShape sphere = new Sphere(15.0);
+
+        System.out.println("cube.getVolume() = " + cube.getVolume());
+        System.out.println("sphere.getVolume() = " + sphere.getVolume());
     }
 }
